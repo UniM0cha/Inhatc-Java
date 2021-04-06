26f82da92d7fe390d09e93ddc8d265b2bc3edc58
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-04-06 14:02:09 +0900

    6주차 수업

---
+++ JavaGUI/src/swing/components/CheckBoxEx.java
@@ -0,0 +1,103 @@
+package swing.components;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class CheckBoxEx extends JFrame implements ItemListener{
+
+	private JCheckBox chk1, chk2, chk3;
+	private JLabel lblSum;
+	
+	public CheckBoxEx(String title, int width, int height) {
+		this.setTitle(title);
+		this.setSize(width, height);
+		this.setLocationRelativeTo(this);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JPanel Center = new JPanel();
+		add(Center,BorderLayout.CENTER);
+		
+		JLabel lbl1 = new JLabel("사과 100원, 배 500원, 체리 2000원");
+		
+		chk1 = new JCheckBox("사과");
+		chk1.addItemListener(this);
+		chk1.setBorderPainted(true);
+		
+		chk2 = new JCheckBox("배");
+		chk2.addItemListener(this);
+		chk2.setBorderPainted(true);
+		
+		chk3 = new JCheckBox("체리");
+		chk3.addItemListener(this);
+		chk3.setBorderPainted(true);
+		
+		lblSum = new JLabel("현재 0원 입니다.");
+		
+		Center.add(lbl1);
+		Center.add(chk1);
+		Center.add(chk2);
+		Center.add(chk3);
+		Center.add(lblSum);
+		
+		JPanel Bottom = new JPanel();
+		add(Bottom,BorderLayout.SOUTH);
+		
+		ButtonGroup bg = new ButtonGroup();
+		JRadioButton rbMan = new JRadioButton("남자");
+		JRadioButton rbWoman = new JRadioButton("여자");
+		
+		bg.add(rbMan);
+		bg.add(rbWoman);
+		
+		Bottom.add(rbMan);
+		Bottom.add(rbWoman);
+		
+		
+		
+		
+		
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new CheckBoxEx("체크박스 연습",250,150);
+	}
+	
+	int sum = 0;
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		
+		Object obj = e.getItem();
+		
+		if(e.getStateChange() == ItemEvent.SELECTED) {
+			if (obj == chk1) {
+				sum += 100;
+			}else if(obj == chk2) {
+				sum += 500;
+			}else if(obj == chk3) {
+				sum += 2000;
+			}
+		}else {
+			if (obj == chk1) {
+				sum -= 100;
+			}else if(obj == chk2) {
+				sum -= 500;
+			}else if(obj == chk3) {
+				sum -= 2000;
+			}
+		}
+
+		lblSum.setText("현재 " + sum + "원 입니다.");
+	}
+
+}
---
+++ JavaGUI/src/swing/components/JLabelEx.java
@@ -0,0 +1,51 @@
+package swing.components;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class JLabelEx extends JFrame{
+	
+	public JLabelEx(String title, int width, int height) {
+		this.setTitle(title);
+		this.setSize(width, height);
+		this.setLocationRelativeTo(this);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+
+		//System.out.println(System.getProperty("user.dir"));
+		
+		ImageIcon iconTel = new ImageIcon("images/tel.png");
+		JLabel lblImg = new JLabel(iconTel);
+		add(lblImg);
+		
+		ImageIcon iconJava = new ImageIcon("images/java.png");
+		JLabel lblImg2 = new JLabel(iconJava);
+		add(lblImg2);
+		
+		ImageIcon arrWhite = new ImageIcon("images/right_arrow_white.png");
+		ImageIcon arrGreen = new ImageIcon("images/right_arrow_green.png");
+		JButton btn = new JButton("버튼", arrWhite);
+		btn.setVerticalTextPosition(JButton.BOTTOM);
+		btn.setRolloverIcon(arrGreen);
+		btn.setPressedIcon(arrGreen);		
+		add(btn);
+		
+		
+		
+		
+		
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new JLabelEx("JLabel 연습",500,300);
+	}
+
+}
