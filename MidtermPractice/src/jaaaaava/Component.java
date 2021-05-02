ce7c8093e91ac71fa2bd58bced7b48bc4200134b
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-05-02 19:13:05 +0900

    Update Component.java

--- MidtermPractice/src/jaaaaava/Component.java
+++ MidtermPractice/src/jaaaaava/Component.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -16,7 +15,6 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import javax.swing.ButtonGroup;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -31,7 +29,6 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 class One extends JFrame{
 	
