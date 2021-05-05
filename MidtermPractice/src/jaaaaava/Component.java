009efdc287e1595a14f4c3b06180dbb8fed6d75c
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-05-05 22:33:53 +0900

    testissue2

--- MidtermPractice/src/jaaaaava/Component.java
+++ MidtermPractice/src/jaaaaava/Component.java
@@ -69,7 +69,16 @@ class One extends JFrame {
 		
 		
 		//issue2
-		
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
+		System.out.println("테스트 중입니다.");
 		
 		
 		
